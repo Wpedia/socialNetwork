57c3c8c692290ae7b296c2cc8ba9e3b6a37e479c
Wpedia <119727989+Wpedia@users.noreply.github.com>
2024-03-29 14:29:19 +0700

    1st part - backend compited

---
+++ Backend/middleware/protectRoute.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken"
+import User from "../models/user.models.js"
+
+const protectRoute = async (req, res, next) => {try {
+    const token = req.cookies.jwt
+    if (!token) {
+        return res.status(401).json({ error: "Unautorized - No token provided"})
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    
+    if (!decoded) {
+        return res.status(401).json({ error: "Unautorized - invalid provided"})
+    }
+
+    const user = await User.findById(decoded.userId)
+
+    if (!user) {
+        return res.status(404).json({ error: "User not found"})
+    }
+
+    req.user = user
+
+    next()
+
+} catch (error) {
+    console.log("error in Protect ", error.message)
+    res.status(500).json({ error: "Internal server error"})
+}}
+
+export default protectRoute
