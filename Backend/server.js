b448903124563334c77c76348599ae3943c5c52c
Wpedia <119727989+Wpedia@users.noreply.github.com>
2024-06-12 20:59:41 +0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -32,10 +32,10 @@ app.use('/api/users', userRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/post', postRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
+app.use(express.static(path.join(__dirname, "/Frontend/dist")))
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+  res.sendFile(path.join(__dirname, "Frontend", "dist", "index.html"))
 })
 
 server.listen(PORT, () => {
